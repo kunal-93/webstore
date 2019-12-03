1679348200a19d4bef7fa635d0fe4311f2816cb4
Kunal Dhawan <n01388885@nj-00118-021.humber.org>
2019-12-03 14:30:51 -0500

    added color radio buttons

--- js/index.js
+++ js/index.js
@@ -10,7 +10,9 @@ let productList =  [
             "category" : "laptop",
             "type" : "Regular",
             "screenSize" : "23.8",
-            "color" : ["Silver", "Black"]
+            "color" : ["Silver", "Black"],
+            "rating": 4.4,
+            "ratingCount" : 200
         },
         {
             "id" : 2,
@@ -24,7 +26,9 @@ let productList =  [
             "category" : "laptop",
             "type" : "Gaming",
             "screenSize" : "15.6",
-            "color" : ["Silver", "Black"]   
+            "color" : ["Silver", "Black"],
+            "rating": 4.7,
+            "ratingCount" : 10
         },
         {
             "id" : 3,
@@ -36,7 +40,9 @@ let productList =  [
             "quantity (in stock)" : 20,
             "category" : "laptop",
             "screenSize" : "15.6",
-            "color" : ["Silver", "Black", "Red"]        
+            "color" : ["Silver", "Black", "Red"],
+            "rating": 4.9,
+            "ratingCount" : 26    
         },
         {
             "id" : 4,
@@ -48,48 +54,70 @@ let productList =  [
             "quantity (in stock)" : 20,
             "category" : "laptop",
             "screenSize" : "15.6",
-            "color" : ["Silver", "Black", "Red"]        
+            "color" : ["Silver", "Black", "Red"],
+            "rating": 3.3,
+            "ratingCount" : 570 
         }
     ]
 
+    // fetch(endPoint)
+    // .then(function(res){
+    //   res.json()
+    //     .then(function(data){
+    //         alert(`Temp is ${data.current.temperature}`);
+    //     })
+  
+    // })
+// <dd>4.4 <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_half</span></dd>
 const getProductAsHTML = (product) => {
-
     return `
     <article class="product">
         <header>
         <img src=${product.image} alt="Product Image">
         </header>
         <h3>${product.name}</h3>
-        <p>Here is a shot of this product that might entice a user to click and add it to their cart.</p>
-        <form>
-        <fieldset>
-            <legend>Colours</legend>
-            <ul>
-            <li><label><input type="radio" name="colour" value="r"> <span>Red</span></label></li>
-            <li><label><input type="radio" name="colour" value="w"> <span>White</span></label></li>
-            <li><label><input type="radio" name="colour" value="b"> <span>Blue</span></label></li>
-            </ul>
-        </fieldset>
+        <form class="product-form">
+        <div class="product-color-sizes">
+            <fieldset>
+                <legend>colour variants</legend>
+                <ul class="swatches">
+                    <li>
+                        <label class="product-color red">
+                            <input type="radio" name="colour" value="r" checked> <!-- default -->
+                            <span>Red</span>
+                        </label>
+                    </li>
 
-        <fieldset>
-            <legend>Sizes</legend>
-            <ol>
-            <li><label><input type="radio" name="size" value="m"> <span>M</span></label></li>
-            <li><label><input type="radio" name="size" value="l"> <span>L</span></label></li>
-            <li><label><input type="radio" name="size" value="xl"> <span>XL</span></label></li>
-            </ol>
-        </fieldset>
+                    <li>
+                        <label class="product-color white">
+                            <input type="radio" name="colour" value="w">
+                            <span>White</span>
+                        </label>
+                    </li>
 
-        <footer>
-            <dl>
-            <dt>Rating</dt>
-            <dd>4.4 <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_half</span></dd>
-            </dl>
-            <data value="39"><del>$50.00</del> <ins>$39.00</ins></data>
-            <button type="button"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
-            <button type="button"><span class="material-icons">favorite</span></button>
-            <a href="#">see more</a>
-        </footer>
+                    <li>
+                        <label class="product-color blue">
+                            <input type="radio" name="colour" value="b"> 
+                            <span>Blue</span>
+                        </label>
+                    </li>
+                </ul>
+                <output id="selected"></output>
+            </fieldset>
+            <fieldset>
+                <legend>Sizes</legend>
+                <ol>
+                    <li><label><input type="radio" name="size" value="m"> <span>M</span></label></li>
+                    <li><label><input type="radio" name="size" value="l"> <span>L</span></label></li>
+                    <li><label><input type="radio" name="size" value="xl"> <span>XL</span></label></li>
+                </ol>
+            </fieldset>
+        </div>
+            <footer>
+                <data value="39"><del>$50.00</del> <ins>$39.00</ins></data>
+                <button type="button"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
+                <button type="button"><span class="material-icons">favorite</span></button>
+            </footer>
         </form>
     </article>`
 
@@ -100,4 +128,3 @@ window.addEventListener("load", () => {
     const productsSection = document.getElementById("products");
     productsSection.innerHTML += productList.map(getProductAsHTML);
 });  
-  
