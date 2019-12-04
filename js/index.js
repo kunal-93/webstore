4c52f984d1b5eba45e42f14090621788c3ad712a
kunal <knnauls@gmail.com>
2019-12-04 17:54:24 -0500

    finished styling draft 1

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let productList =  [
             "description" : ["Windows 10 Home", "Intel® Core™ i5-7400T processor Quad-core 2.40 GHz", "17.3\" Full HD (1920 x 1080) 16:9 Touchscreen",
                             "Intel® HD Graphics 630 shared memory", "8 GB, DDR4 SDRAM", "1 TB HDD"],
             "price" : 899.99,
-            "quantity (in stock)" : 10,
+            "quantity" : 10,
             "category" : "laptop",
             "type" : "Regular",
             "screenSize" : "23.8",
@@ -22,7 +22,7 @@ let productList =  [
                             "NVIDIA® GeForce® GTX 1060 with 6 GB Dedicated Memory", "16 GB, DDR4 SDRAM", "1 TB HDD, 256 GB SSD",
                             "30-day Microsoft Office trial included"],
             "price" : 1499.99,
-            "quantity (in stock)" : 15,
+            "quantity" : 15,
             "category" : "laptop",
             "type" : "Gaming",
             "screenSize" : "15.6",
@@ -37,7 +37,7 @@ let productList =  [
             "description" : ["Windows 10 Home", "Intel® Core™ i7-8750H processor Quad-core 2.40 GHz", "15.6\" Full HD (1920 x 1080) 16:9",
                             "NVIDIA® GeForce® GTX 1050 with 4 GB Dedicated Memory", "8 GB, DDR4 SDRAM", "1 TB HDD"],
             "price" : 899.99,
-            "quantity (in stock)" : 20,
+            "quantity" : 20,
             "category" : "laptop",
             "screenSize" : "15.6",
             "color" : ["Silver", "Black", "Red"],
@@ -51,7 +51,7 @@ let productList =  [
             "description" : ["Windows 10 Home", "Intel® Core™ i5-8300H processor Quad-core 2.30 GHz", "15.6\" Full HD (1920 x 1080) 16:9",
                             "NVIDIA® GeForce® GTX 1050 with 4 GB Dedicated Memory", "8 GB, DDR4 SDRAM", "1 TB HDD"],
             "price" : 799.99,
-            "quantity (in stock)" : 20,
+            "quantity" : 20,
             "category" : "laptop",
             "screenSize" : "15.6",
             "color" : ["Silver", "Black", "Red"],
@@ -60,6 +60,28 @@ let productList =  [
         }
     ]
 
+const getProductStatusbyQtyAsHTML = (quantity) => {
+    let colorClass = null;
+    let stockStatus = null;
+    if(quantity>10){
+        colorClass = `green-color`;
+        stockStatus = `In Stock`;
+    }
+    else if(quantity>0){
+        colorClass = `red-color`;
+        stockStatus = `Hurry, only ${quantity} left`;
+    }
+    else{
+        colorClass = `grey-color`;
+        stockStatus = `Out of Stock`;
+    }
+
+    return `<span class=${colorClass}>${stockStatus}</span>`
+};
+
+const getProductDescriptionAsListItems = (descriptionPoint) => {
+    return `<li>${descriptionPoint}</li>`;
+}
     // fetch(endPoint)
     // .then(function(res){
     //   res.json()
@@ -72,53 +94,51 @@ let productList =  [
 const getProductAsHTML = (product) => {
     return `
     <article class="product">
-        <header>
-        <img src=${product.image} alt="Product Image">
+        <header class="bottom-border">
+            <img src=${product.image} alt="Product Image">
+            <div class="product-header-grid">
+                ${getProductStatusbyQtyAsHTML(product.quantity)}
+                <fieldset class="product-colors-block">
+                    <legend>colour variants</legend>
+                    <ul class="swatches">
+                        <li>
+                            <label class="product-color red">
+                                <input type="radio" name="colour" value="r" checked> <!-- default -->
+                                <span>Red</span>
+                            </label>
+                        </li>
+
+                        <li>
+                            <label class="product-color white">
+                                <input type="radio" name="colour" value="w">
+                                <span>White</span>
+                            </label>
+                        </li>
+
+                        <li>
+                            <label class="product-color blue">
+                                <input type="radio" name="colour" value="b"> 
+                                <span>Blue</span>
+                            </label>
+                        </li>
+                    </ul>
+                    <output id="selected"></output>
+                </fieldset>
+            </div>
         </header>
-        <h3>${product.name}</h3>
-        <form class="product-form">
-        <div class="product-color-sizes">
-            <fieldset>
-                <legend>colour variants</legend>
-                <ul class="swatches">
-                    <li>
-                        <label class="product-color red">
-                            <input type="radio" name="colour" value="r" checked> <!-- default -->
-                            <span>Red</span>
-                        </label>
-                    </li>
-
-                    <li>
-                        <label class="product-color white">
-                            <input type="radio" name="colour" value="w">
-                            <span>White</span>
-                        </label>
-                    </li>
-
-                    <li>
-                        <label class="product-color blue">
-                            <input type="radio" name="colour" value="b"> 
-                            <span>Blue</span>
-                        </label>
-                    </li>
-                </ul>
-                <output id="selected"></output>
-            </fieldset>
-            <fieldset>
-                <legend>Sizes</legend>
-                <ol>
-                    <li><label><input type="radio" name="size" value="m"> <span>M</span></label></li>
-                    <li><label><input type="radio" name="size" value="l"> <span>L</span></label></li>
-                    <li><label><input type="radio" name="size" value="xl"> <span>XL</span></label></li>
-                </ol>
-            </fieldset>
-        </div>
-            <footer>
-                <data value="39"><del>$50.00</del> <ins>$39.00</ins></data>
-                <button type="button"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
-                <button type="button"><span class="material-icons">favorite</span></button>
-            </footer>
-        </form>
+        <main class="bottom-border">
+            <h3>${product.name}</h3>
+            <ul class="product-description-list">
+                ${product.description.map(getProductDescriptionAsListItems).join('')}
+            </ul>
+        </main>
+        <footer class="bottom-border">
+            <form class="product-footer-form">
+                <data class="price" value="39"><del>$50.00</del><ins class="offer-price">$39.00</ins></data>
+                <input type="button" class="buy-button" value="Buy Now">
+                <button class="light-buttons"><span class="material-icons add-cart">add_shopping_cart</span></button>
+            </form>
+        </footer>
     </article>`
 
     return HTMLString;
